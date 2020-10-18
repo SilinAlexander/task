34b12a54cbe4a691a66b37cec0a520d345a17ebe
SilinAlexander <asilin1997@mail.ru>
2020-10-18 10:42:31 +0500

    4

--- task/task/settings.py
+++ task/task/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
