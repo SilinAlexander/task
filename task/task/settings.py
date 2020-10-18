a19b4321b26ca6e001dc2d3d12151d1e689e229a
SilinAlexander <asilin1997@mail.ru>
2020-10-18 18:26:22 +0500

    12

--- task/task/settings.py
+++ task/task/settings.py
@@ -131,4 +131,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
-
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
